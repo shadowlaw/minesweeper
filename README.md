cae3cb1e29cee1ed6d3029a62369db324f75bccf
Javed Wright <jdwright870@gmail.com>
2024-08-18 11:43:06 -0500

    application setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Minesweeper 0.0.0
+
+A Simple desktop minesweeper game. 
---
+++ src/main/java/com/shadowlaw/minesweeper/Main.java
@@ -0,0 +1,13 @@
+package com.shadowlaw.minesweeper;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+
+    private static Logger logger = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args){
+        logger.info("Application Run");
+    }
+}
