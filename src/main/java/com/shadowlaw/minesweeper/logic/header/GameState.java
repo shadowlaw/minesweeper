f6b19dc1c97c38287bf6bfe6cc9a4f51eb5d7965
Javed Wright <jdwright870@gmail.com>
2024-09-15 07:35:23 -0500

    [refactor] move game state attributes into game state object

--- src/main/java/com/shadowlaw/minesweeper/logic/header/GameState.java
+++ src/main/java/com/shadowlaw/minesweeper/logic/header/GameState.java
@@ -4,6 +4,8 @@ import com.shadowlaw.minesweeper.ui.components.GameStateIcon;
 
 public class GameState {
 
+    private Boolean isStarted = Boolean.FALSE;
+    private boolean isGameStatePlayable = true;
     private Boolean gameWinState = null;
     private GameStateIcon gameStateIcon;
 
@@ -18,5 +20,19 @@ public class GameState {
         }
     }
 
+    public boolean isGameStartable() {
+        return !isStarted && isGameStatePlayable;
+    }
 
+    public void setStarted(Boolean started) {
+        isStarted = started;
+    }
+
+    public boolean isGameStatePlayable() {
+        return isGameStatePlayable;
+    }
+
+    public void setGameStatePlayable(boolean gameStatePlayable) {
+        isGameStatePlayable = gameStatePlayable;
+    }
 }
